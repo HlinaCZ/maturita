3e83e514c387fc52d0d3b1fd8cfa2efb5b809fa2
HlinaCZ <djhlinacz@gmail.com>
2019-12-19 08:23:34 +0100

    Added topic 10
    Signed-off-by: HlinaCZ <djhlinacz@gmail.com>

--- 10-txt-soubory/vrahova_vypoved/main.cpp
+++ 10-txt-soubory/vrahova_vypoved/main.cpp
@@ -1,9 +1,38 @@
+#include <fstream>
 #include <iostream>
 
-using namespace std;
+int main() {
+  std::string line;
 
-int main()
-{
-  cout << "Hello World!" << endl;
+  std::ifstream original("/home/hlina/Programming/School/maturita/"
+                         "10-txt-soubory/vrahova_vypoved/vypoved.txt");
+  std::ofstream result("/home/hlina/Programming/School/maturita/"
+                       "10-txt-soubory/vrahova_vypoved/skutecnost.txt");
+
+  if (!original.is_open()) {
+    std::cout << "Vypoved.txt se nepodarilo nacist" << std::endl;
+    return 0;
+  }
+
+  if (!result.is_open()) {
+    std::cout << "Skutecnost.txt se nepodarilo nacist" << std::endl;
+    return 0;
+  }
+
+  while (getline(original, line)) {
+    // odstraneni ne na zacatku vsech slov
+    std::string::size_type pos;
+    do {
+      // pozice nalezeneneho " ne"
+      pos = line.find(" ne");
+      if (pos != std::string::npos)
+        line.erase(pos + 1, 2);
+    } while (pos != std::string::npos);
+
+    result << line << std::endl;
+  }
+
+  original.close();
+  result.close();
   return 0;
 }
